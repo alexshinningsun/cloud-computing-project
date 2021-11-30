8a29b582056b2011a944256e575b1b45d55ba001
alexshinningsun <46494614+alexshinningsun@users.noreply.github.com>
2021-11-30 21:05:58 +0800

    Update outputs.tf

--- up-ec2-terraform/outputs.tf
+++ up-ec2-terraform/outputs.tf
@@ -7,3 +7,8 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.app_server.*.public_ip
 }
+
+output "pem_key" {
+  description = "Private key of the EC2 instance"
+  value       = tls_private_key.example.private_key_pem
+}
