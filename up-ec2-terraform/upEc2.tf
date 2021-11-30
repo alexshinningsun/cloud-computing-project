e552ca019d430dbfae2906b573a684887e653867
alexshinningsun <46494614+alexshinningsun@users.noreply.github.com>
2021-11-30 18:01:05 +0800

    Update upEc2.tf

--- up-ec2-terraform/upEc2.tf
+++ up-ec2-terraform/upEc2.tf
@@ -19,7 +19,7 @@ resource "aws_iam_role" "ec2_role" {
 
   assume_role_policy = <<EOF
 {
-  "Version": "2021-10-17",
+  "Version": "2012-10-17",
   "Statement": [
     {
       "Action": "sts:AssumeRole",
